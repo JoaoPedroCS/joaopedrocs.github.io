0c4683eff3587fd1b707ab91e89087e4a2efb071
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-09-08 23:47:35 -0300

    a

--- calculadora/calcdir/calc.js
+++ calculadora/calcdir/calc.js
@@ -145,6 +145,24 @@ function negative() {
     }
 }
 
+function moveDecimalLeft(numStr) {
+    let decimalIndex = numStr.indexOf('.');
+  
+    if (decimalIndex === -1) {
+      // Se não houver ponto, adicionar no final e mover uma casa para a esquerda
+      return numStr.slice(0, -2) + '.' + numStr.slice(-2) + numStr.slice(-1);
+    } else if (decimalIndex === 0) {
+      // Se o ponto estiver no início, retorná-lo como 0.X
+      return '0' + numStr;
+    } else {
+      // Remover o ponto e colocá-lo uma posição à esquerda
+      let beforeDecimal = numStr.slice(0, decimalIndex - 2);
+      let afterDecimal = numStr.slice(decimalIndex - 2) + numStr.slice(decimalIndex - 1);
+      return beforeDecimal + '.' + afterDecimal.replace('.', '');
+    }
+  }
+  
+
 function percentage() {
     if (!selectedOperatorButton) {
         firstinput = String(0.01* Number(firstinput))
