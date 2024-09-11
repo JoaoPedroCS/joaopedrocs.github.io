d4de2db5d4b0646f2d22fb42302ffc588e66f7c7
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-09-11 16:05:52 -0300

    aa

--- to-do/tododir/todo.js
+++ to-do/tododir/todo.js
@@ -1,1 +1,33 @@
-alert('text')
+const todoform = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
+
+let allTodos = [];
+
+todoform.addEventListener('submit', function(e) {
+    e.preventDefault();
+    addTodo();
+})
+
+function addTodo() {
+    const todoText = todoInput.value.trim();
+    if(todoText.length > 0) {
+        allTodos.push(todoText);
+        updateTodoList();
+        todoInput.value = '';
+    }
+}
+
+function updateTodoList() {
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIndex) => {
+        todoItem = createTodoItem(todo, todoIndex);
+        todoListUL.append(todoItem);
+    })
+}
+
+function createTodoItem(todo) {
+    const todoLI = document.createElement("li");
+    todoLI.innerText = todo;
+    return todoLI
+}
