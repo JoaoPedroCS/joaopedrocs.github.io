ebfff55982a22fad4bd3c57d2a17599d015c0990
João Pedro <72800066+JoaoPedroCS@users.noreply.github.com>
2024-09-11 23:17:46 -0300

    todo once again

--- to-do/tododir/todo.js
+++ to-do/tododir/todo.js
@@ -26,8 +26,25 @@ function updateTodoList() {
     })
 }
 
-function createTodoItem(todo) {
+function createTodoItem(todo, todoIndex) {
+    const todoId = "todo-"+todoIndex;
     const todoLI = document.createElement("li");
-    todoLI.innerText = todo;
+    todoLI.className = "todo";
+    todoLI.innerHTML = `
+    <input type="checkbox" id="${todoId}" class="todo-checkbox">
+                    <label for="${todoId}" class="custom-checkbox"><svg fill="transparent"
+                            xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e8eaed">
+                            <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+                        </svg></label>
+                    <label for="${todoId}" class="todo-text">${todo}</label>
+                    <button class="delete-button">
+                        <svg fill="var(--secondary)" xmlns="http://www.w3.org/2000/svg" height="24px"
+                            viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                            <path
+                                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                        </svg>
+                    </button>
+    `
     return todoLI
 }
